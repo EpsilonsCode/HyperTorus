3dc6d6a24929e53e8844bfcc45e5013dd3959eae
michael <michal.lutomirski@gmail.com>
2024-09-15 12:56:27 +0200

    Add text to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-readme
+Projekt robiony w Intellij Community używając java 22
+program tworzy obracający się hipertorus (aka 4 wymiarowy pączek)
+jako że nie da sie zwizualizować figur powyżej 3 wymiaru to robimy przekrój (cień)
+hipertorusa. Parametry A, B, C, D i E to parametry przekrojowej hiperpłażczyzny do której rzutujemy.
+2 wymiarowym ekwiwalentem hiperpłażczyzny jest linia 3 wymiarowym zwykła płażczyzna.
+Parametry R, T i D to trzy promienie hipertorusa. Reszty nie będę opisywał bo pisze to na szybkości
+(TODO dodać prawdziwy opis).
---
+++ src/main/java/com/omicron/hypertorus/Sandbox.java
@@ -0,0 +1,32 @@
+package com.omicron.hypertorus;
+
+public class Sandbox {
+
+
+
+    public static void main(String[] args) {
+        int fatAss = 0;
+        System.out.println(fib(5));
+        pala();
+    }
+
+    public static int fib(int n)
+    {
+        return n > 1 ? fib(n - 1) * n : 1;
+    }
+
+    public static int pala()
+    {
+        String s = "kdfjsdfjkfajdjaskkkkkkd";
+        int sayGex = 01312;
+        int gaySex = 0x101;
+        System.out.printf(sayGex + " " + gaySex);
+        float sugOnMaDick;
+
+        return 0;
+    }
+}
+
+class Comlex {
+    private double x, y;
+}
